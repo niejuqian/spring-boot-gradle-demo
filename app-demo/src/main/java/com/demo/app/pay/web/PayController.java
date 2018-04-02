84fe113e6e2156733e8f4f0e589f79110f5d7300
6213 <6213@cunpiao.com>
2018-04-02 20:07:38 +0800

    优化

--- app-demo/src/main/java/com/demo/app/pay/web/PayController.java
+++ app-demo/src/main/java/com/demo/app/pay/web/PayController.java
@@ -1,14 +1,17 @@
 package com.demo.app.pay.web;
 
+import com.demo.common.util.JsonUtil;
 import com.demo.common.util.SpringContextUtil;
 import com.demo.common.util.XmlHelpUtil;
 import com.demo.pay.dto.PayRequest;
 import com.demo.pay.dto.PayResponse;
 import com.demo.pay.service.AbstractPayService;
+import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,6 +19,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  *  支付
@@ -70,5 +75,32 @@ public class PayController {
         }
         logger.info("===========>>>>微信支付回调响应参数：{}",resultBody);
         return resultBody;
+    }
+
+
+    /**
+     * 支付宝支付回调
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    @PostMapping("/aliPayNotify")
+    @ResponseBody
+    public String aliPayNotify(HttpServletRequest request) throws Exception {
+        String body = "";
+        Map<String, String> params = Maps.newHashMap();
+        Map<String,String[]> requestParams = request.getParameterMap();
+        for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext(); ) {
+            String name = (String) iter.next();
+            String[] values = requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i++) {
+                valueStr = (i == values.length - 1) ? valueStr + values[i] : valueStr + values[i] + ",";
+            }
+            params.put(name, valueStr);
+        }
+        body = JsonUtil.beanToJson(params);
+        logger.info("===========>>>>支付宝支付回调参数：{}",body);
+        return "success";
     }
 }
